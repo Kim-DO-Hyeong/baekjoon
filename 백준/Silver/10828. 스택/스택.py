a7d1d4a1967987c9b3b46c2b5f7a5d81cb0e08fb
Kim-DO-Hyeong <105036110+Kim-DO-Hyeong@users.noreply.github.com>
2023-07-01 19:03:39 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,24 +1,42 @@
-import sys
-stack = []
 
-for i in range(int(sys.stdin.readline())):
-    order = sys.stdin.readline().split()
-    if order[0] == "push":
-        stack.append(order[1])
-    if order[0] == "pop":
-        if len(stack) == 0:
-            print(-1)
-        else:
-            print(stack.pop())
-    if order[0] == "size":
-        print(len(stack))
-    if order[0] == "empty":
-        if len(stack) == 0:
-            print(1)
-        else:
-            print(0)
-    if order[0] == "top":
-        if len(stack) == 0:
-            print(-1)
-        else:
-            print(stack[-1])
+import sys
+a=[]
+def push(x):
+    a.append(x)
+
+def pop():
+    if len(a) == 0:
+        print(-1)
+    else:
+        print(a.pop(-1))
+
+def size():
+    print(len(a))
+
+def empty():
+    if len(a) == 0:
+        print(1)
+    else:
+        print(0)
+
+def top():
+    if len(a) == 0:
+        print(-1)
+    else:
+        print(a[-1])
+
+n = int(sys.stdin.readline())
+
+for s in range(n):
+    command = sys.stdin.readline()
+    i = command.split()
+    if i[0] == 'push':
+        push(int(i[1]))
+    elif i[0] == 'size':
+        size()
+    elif i[0] == 'empty':
+        empty()
+    elif i[0] == 'pop':
+        pop()
+    elif i[0] == 'top':
+        top()
